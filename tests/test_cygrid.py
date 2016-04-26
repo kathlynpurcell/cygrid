284b70a39044e3b96c78d6d65c13b08006696ed0
Benjamin Winkel <bwinkel@mpifr.de>
2016-04-26 23:22:49 +0200

    tests: add SlGrid test

--- tests/test_cygrid.py
+++ tests/test_cygrid.py
@@ -10,7 +10,7 @@ import pytest
 import numpy as np
 # from numpy.testing import assert_equal, assert_allclose
 # from astropy.utils.misc import NumpyRNGContext
-from cygrid import WcsGrid
+from cygrid import WcsGrid, SlGrid
 
 
 class TestWcsGrid:
@@ -78,3 +78,59 @@ class TestWcsGrid:
             )
 
         mygridder.grid(self.xcoords, self.ycoords, self.signal[:, np.newaxis])
+
+
+class TestSlGrid:
+
+    def setup(self):
+
+        mapcenter = (131., 50.)
+        mapsize = (1., 1.)  # degrees
+        self.beamsize_fwhm = 0.05  # degrees
+
+        avg_num_pixels_x = 5 * mapsize[0] / self.beamsize_fwhm  # per dimension
+        avg_num_pixels_y = 5 * mapsize[1] / self.beamsize_fwhm  # per dimension
+
+        self.input_x = np.random.uniform(
+            mapcenter[0] - mapsize[0]/2.,
+            mapcenter[0] + mapsize[0]/2.,
+            avg_num_pixels_x * avg_num_pixels_y,
+            ).astype(np.float64)
+        self.input_y = np.random.uniform(
+            mapcenter[1] - mapsize[1]/2.,
+            mapcenter[1] + mapsize[1]/2.,
+            avg_num_pixels_x * avg_num_pixels_y,
+            ).astype(np.float64)
+        self.signal = np.random.normal(0., .01, len(self.input_x))
+
+        self.target_x = np.random.uniform(
+            mapcenter[0] - mapsize[0]/2.,
+            mapcenter[0] + mapsize[0]/2.,
+            1000,
+            ).astype(np.float64)
+        self.target_y = np.random.uniform(
+            mapcenter[0] - mapsize[0]/2.,
+            mapcenter[0] + mapsize[0]/2.,
+            1000,
+            ).astype(np.float64)
+
+    def teardown(self):
+
+        pass
+
+    def test_gridding(self):
+
+        kernelsize_fwhm = self.beamsize_fwhm / 2
+        kernelsize_sigma = kernelsize_fwhm / 2.35
+        support_radius = 3. * kernelsize_sigma
+        hpx_min_res = kernelsize_sigma / 2.
+
+        mygridder = SlGrid(self.target_x, self.target_y, 1)
+        mygridder.set_kernel(
+            'gauss1d',
+            (0.5 / kernelsize_sigma ** 2,),
+            support_radius,
+            hpx_min_res,
+            )
+
+        mygridder.grid(self.input_x, self.input_y, self.signal[:, np.newaxis])
