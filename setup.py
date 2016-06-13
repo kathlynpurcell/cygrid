2562183320c1817382a8febe786669b3c8441513
bwinkel <bwinkel78@gmail.com>
2016-06-13 10:42:32 +0200

    replace distutils by setuptools to improve dependency handling, fixes #2

--- setup.py
+++ setup.py
@@ -1,43 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
-from distutils.extension import Extension
-from distutils.version import StrictVersion, LooseVersion
+from setuptools import setup
+from setuptools.extension import Extension
 from Cython.Distutils import build_ext
-import warnings
-import sys
 import numpy
 import platform
+
 
 EX_COMP_ARGS = []
 if 'mac' in platform.system().lower():
     EX_COMP_ARGS += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-
-
-# Dependency checking
-dependencies = [['cython', '0.20.2'], ['numpy', '1.8'], ['astropy', '1.0']]
-
-for (pkg, minversion) in dependencies:
-    try:
-        m = __import__(pkg)
-        if minversion is not None:
-            if StrictVersion(m.__version__) < StrictVersion(minversion):
-                if LooseVersion(m.__version__) < LooseVersion(minversion):
-                    raise ValueError
-                warnings.warn(
-                    'Version', m.__version__,
-                    'of package', pkg,
-                    'might not be sufficient'
-                    )
-    except ImportError:
-        print('Package', pkg, 'not present.')
-        sys.exit(1)
-    except ValueError:
-        print('Package', pkg, 'has version', m.__version__)
-        print('Version', minversion, 'required.')
-        sys.exit(1)
-
 
 GRID_EXT = Extension(
     'cygrid.cygrid',
@@ -96,13 +69,19 @@ KERNEL_EXT = Extension(
 
 setup(
     name='cygrid',
-    version='0.9.4',
+    version='0.9.5',
     author='Benjamin Winkel, Lars Flöer, Daniel Lenz',
     author_email='bwinkel@mpifr.de',
     description=(
         'Cygrid is a cython-powered convolution-based gridding module '
         'for astronomy'
         ),
+    install_requires=[
+        'setuptools',
+        'cython>=0.20.2',
+        'numpy>=1.8',
+        'astropy>=1.0',
+    ],
     packages=['cygrid'],
     cmdclass={'build_ext': build_ext},
     ext_modules=[
