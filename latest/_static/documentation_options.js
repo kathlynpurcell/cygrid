64c199bac478eca51da298fa3be03c9819bd92f0
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-12-02 23:03:25 +0000

    Update docs after building Travis build 131 of bwinkel/cygrid
    The docs were built from the branch 'apt' against the commit
    c1da949995b032c443aa62c5c38daf207d390d00.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bwinkel/cygrid/jobs/462578921.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test/bin/doctr deploy --no-require-master --sync --built-docs docs/_build/html latest

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0dev50',
+    VERSION: '1.0.0dev49',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
