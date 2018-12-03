74cc53df3036abd13cdd08ef8b60efee7263fc3e
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-12-03 22:41:52 +0000

    Update docs after building Travis build 133 of bwinkel/cygrid
    The docs were built from the branch 'apt' against the commit
    53d77e3f5a729c77f2c8cd0396ef34a68eebb91d.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bwinkel/cygrid/jobs/463052322.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test/bin/doctr deploy --no-require-master --sync --built-docs docs/_build/html latest

--- latest/user_manual-1.py
+++ latest/user_manual-1.py
@@ -1,10 +1,17 @@
+# Example adapted from "scipy.interpolate.griddata" docs
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 
 
 def func(x, y):
-    return x*(1-x)*np.cos(4*np.pi*x) * np.sin(4*np.pi*y**2)**2
+
+    return (
+        x * (1 - x) *
+        np.cos(4 * np.pi * x) *
+        np.sin(4 * np.pi * y ** 2) ** 2
+        )
 
 grid_x, grid_y = np.mgrid[0:1:100j, 0:1:200j]
 
@@ -12,10 +19,18 @@ grid_x, grid_y = np.mgrid[0:1:100j, 0:1:200j]
 points = np.random.rand(1000, 2)
 values = func(points[:,0], points[:,1])
 
-gridded = griddata(points, values, (grid_x, grid_y), method='cubic')
+gridded = griddata(
+    points,
+    values,
+    (grid_x, grid_y),
+    method='cubic'
+    )
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-ax1.imshow(func(grid_x, grid_y).T, extent=(0,1,0,1), origin='lower')
+ax1.imshow(
+    func(grid_x, grid_y).T,
+    extent=(0,1,0,1), origin='lower'
+    )
 ax1.plot(points[:,0], points[:,1], 'k.', ms=1)
 ax1.set_title('Original')
 ax2.imshow(gridded.T, extent=(0,1,0,1), origin='lower')
