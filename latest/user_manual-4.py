7d6a2317a8d0bd8186c2026d920dd873164230ea
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-12-12 15:32:58 +0000

    Update docs after building Travis build 139 of bwinkel/cygrid
    The docs were built from the branch 'apt' against the commit
    02ba7aa30f92f7dcd2dd90b8c5259854c4a9b235.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/bwinkel/cygrid/jobs/467048548.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test/bin/doctr deploy --no-require-master --sync --built-docs docs/_build/html latest

---
+++ latest/user_manual-4.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.wcs import WCS
+from astropy.utils.misc import NumpyRNGContext
+import cygrid
+
+mapcenter = 60., 30.  # all in degrees
+mapsize = 5., 5.
+beamsize_fwhm = 0.1
+num_samples = 10 ** 6
+num_sources = 20
+
+with NumpyRNGContext(1):
+    lons, lats, signal = cygrid.produce_mock_data(
+        mapcenter, mapsize, beamsize_fwhm, num_samples, num_sources
+        )
+
+pixsize = beamsize_fwhm / 3.
+dnaxis1 = int(mapsize[0] / pixsize)
+dnaxis2 = int(mapsize[1] / pixsize)
+
+target_header = {
+    'NAXIS': 3,
+    'NAXIS1': dnaxis1,
+    'NAXIS2': dnaxis2,
+    'NAXIS3': 1,  # need dummy spectral axis
+    'CTYPE1': 'RA---SIN',
+    'CTYPE2': 'DEC--SIN',
+    'CUNIT1': 'deg',
+    'CUNIT2': 'deg',
+    'CDELT1': -pixsize,
+    'CDELT2': pixsize,
+    'CRPIX1': dnaxis1 / 2.,
+    'CRPIX2': dnaxis2 / 2.,
+    'CRVAL1': mapcenter[0],
+    'CRVAL2': mapcenter[1],
+    }
+
+gridder = cygrid.WcsGrid(target_header)
+
+kernelsize_fwhm = 2.5 / 60.  # degrees
+kernelsize_sigma = kernelsize_fwhm / np.log(8 * np.sqrt(2))
+sphere_radius = 3. * kernelsize_sigma
+
+gridder.set_kernel(
+    'gauss1d',
+    (kernelsize_sigma,),
+    sphere_radius,
+    kernelsize_sigma / 2.
+    )
+gridder.grid(lons, lats, signal[:, np.newaxis])
+gridded_map = gridder.get_datacube().squeeze()
+
+target_wcs = gridder.get_wcs()
+
+fig = plt.figure(figsize=(7, 7))
+ax = fig.add_subplot(111, projection=target_wcs.celestial)
+im = ax.imshow(
+    gridded_map, vmin=-0.5, vmax=8.0,
+    origin='lower', interpolation='nearest'
+    )
+lon, lat = ax.coords
+lon.set_axislabel('R.A. [deg]')
+lat.set_axislabel('Dec [deg]')
+plt.show()
